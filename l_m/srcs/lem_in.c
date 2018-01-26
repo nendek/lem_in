1ee88c710af33f3174caf2966cdb35055202ad06
Pierre NARDOZI <pnardozi@e2r3p3.42.fr>
2018-01-26 16:15:47 +0100

    ajouter start comme contrainte

--- l_m/srcs/lem_in.c
+++ l_m/srcs/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: pnardozi <pnardozi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 11:31:48 by pnardozi          #+#    #+#             */
-/*   Updated: 2018/01/26 15:19:10 by pnardozi         ###   ########.fr       */
+/*   Updated: 2018/01/26 16:15:08 by pnardozi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,37 @@ char			*lm_search_s_e(t_room **begin_list, int s_e)
 	return (tmp);
 }
 
+int				lm_coutn_link(t_tunnel **begin_list, char *name)
+{
+	int			i;
+	t_tunnel	*lst;
+
+	lst = *begin_list;
+	i = 0;
+	if (lst)
+		while (lst)
+		{
+			if (ft_strcmp(lst->name1, name) == 0)
+				i++;
+			lst = lst->next;
+		}
+	return (i);
+}
+
 t_tunnel		*lm_search_tunnel(t_tunnel **begin_list, char *name)
 {
 	t_tunnel *lst;
 	int			i;
+	int			links;
 
+	links = lm_coutn_link(begin_list, name);
+	ft_printf("name = %s, link = %d\n", name, links);
 	i = 0;
 	lst = *begin_list;
 	if (lst)
 		while (lst)
 		{
-			if (ft_strcmp(lst->name2, name) == 0 && lst->visit == 0)
+			if (ft_strcmp(lst->name2, name) == 0 && lst->visit <= links)
 			{
 				i = 1;
 				break ;
@@ -55,13 +75,12 @@ int				lm_algo(t_tunnel **begin_list, char *start, char *end)
 {
 	t_tunnel	*tunnel;
 
-	ft_printf("end = %s\n", end);
 	while ((tunnel = lm_search_tunnel(begin_list, end)) != NULL)
 	{
-		tunnel->visit = 1;
+		tunnel->visit += 1;
 		ft_printf("tunnel = %s-%s\n", tunnel->name1, tunnel->name2);
 		if (ft_strcmp(tunnel->name1, start) == 0)
-			ft_printf("saluttututut\n");
+			ft_printf("t as trouver start encule\n");
 		lm_algo(begin_list, start, tunnel->name1);
 	}
 	return (0);
