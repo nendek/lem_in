fd5c5505abb8653b8cb7f0f915f0af205d8fb853
Pierre NARDOZI <pnardozi@e1r8p6.42.fr>
2018-02-08 12:05:39 +0100

    faire pour un chemin start-end et norme et leaks

--- l_m/srcs/display.c
+++ l_m/srcs/display.c
@@ -6,7 +6,7 @@
 /*   By: pnardozi <pnardozi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 11:27:44 by pnardozi          #+#    #+#             */
-/*   Updated: 2018/02/07 17:40:30 by pnardozi         ###   ########.fr       */
+/*   Updated: 2018/02/08 12:04:26 by pnardozi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int		lm_count_lst(t_visit **begin_list)
 	return (i);
 }
 
-int		lm_move_ants(t_visit **lst_road, t_room *end_room, int i, t_room **lst_room)
+int		lm_move_ants(t_visit **lst_road, t_room *end_room, int i, t_room **lst_room, int *nb_ants)
 {
 	char 		*start;
 	t_visit		*road1;
@@ -61,7 +61,6 @@ int		lm_move_ants(t_visit **lst_road, t_room *end_room, int i, t_room **lst_room
 			tmp++;
 		}
 		road1 = road2->next;
-		//dprintf(1, "road1 = %s, road2 = %s\n", road1->name, road2->name);
 		if (ft_strcmp(road1->name, end_room->name) == 0)
 		{
 			if (road2->name_ant != 0)
@@ -75,9 +74,10 @@ int		lm_move_ants(t_visit **lst_road, t_room *end_room, int i, t_room **lst_room
 		{
 			if (road1->name_ant == 0 && road2->name != 0)
 			{
-				ft_printf("L%d-%s ", road2->name_ant, road1->name);
-				road1->name_ant = road2->name_ant;
+				ft_printf("L%d-%s ", *nb_ants, road1->name);
+				road1->name_ant = *nb_ants;
 				road2->name_ant--;
+				*nb_ants -= 1;
 			}
 		}
 		else if (road1->name_ant == 0 && road2->name_ant != 0)
@@ -97,14 +97,16 @@ int		lm_display(t_visit **lst_road, t_room **lst_room, int nb_ants)
 {
 	t_room		*room_end;
 	int			i;
+	int			tmp;
 
-	i = nb_ants;
-
-
+	tmp = nb_ants;
 	i = lm_count_lst(lst_road);
 	lm_get_end(lst_room, &(room_end));
+
+	t_visit *lst_tmp;
+	lst_tmp = *lst_road;
 	while (room_end->ants != nb_ants)
-		lm_move_ants(lst_road, room_end, i, lst_room);
+		lm_move_ants(lst_road, room_end, i, lst_room, &tmp);
 
 	return (1);
 }
